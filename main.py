212404980bbe935c6769f55bc1ffb70ea3ac8e19
xvhaorong <hxx5086@psu.edu>
2020-09-18 19:04:24 +0000

    11

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Author: Haorong Xu hxx5086@psu.edu 
+
 def digit_sum(n):
   if(n>0):
     return n%10 + digit_sum(n//10)
   else:
-    return n%10;
+    return n%10
 
-  def run():
-    num=int(input("Enter an int: "))
-    print (f"sum of digits of {num} is {digit_sum(num)}.")
+def run():
+  a=int(input("Enter an int: "))
+  print (f"sum of digits of {a} is {digit_sum(a)}.")
 
-  if __name__ == "__main__":
+if __name__ == "__main__":
     run()
